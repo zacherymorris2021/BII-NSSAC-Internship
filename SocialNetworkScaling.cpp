49f42008d1819352e178a64b5f09398ad7711530
zacherymorris2021 <46734896+zacherymorris2021@users.noreply.github.com>
2019-06-14 10:35:57 -0400

    FinalProgram with CoutTests

--- SocialNetworkScaling.cpp
+++ SocialNetworkScaling.cpp
@@ -7,20 +7,28 @@
 #include <sstream>
 #include <string>
 #include <vector>
-//#include "SocialNetworkScaling.h"
+#include <algorithm>
+#include <random>
+#include <time.h>
 
 using namespace std;
+
+// forward declarations
+vector<int> newInterval(int endRange, int vectorSize);
+
+// global initalization
+int numNodeCopies = 0;
+float rewiringFactor = 0.0;
 
 int main(int argc, char * argv[]) {
     string fileName = argv[1];
 
     // convert nodeCopies string to int
     stringstream numConvert(argv[2]);
-    int nodeCopies = 0;
-    numConvert >> nodeCopies;
+    numConvert >> numNodeCopies;
 
     // convert resizing string to float
-    float resizingFactor = std::stof(argv[3]);
+    rewiringFactor = std::stof(argv[3]);
 
     // try to read in the file
     ifstream myFileStream(fileName);
@@ -34,7 +42,7 @@ int main(int argc, char * argv[]) {
     string line;
     vector<int> sourcePIDs, sourceActivities, targetPIDs, targetActivities, durations;
 
-    // puts 5 data pieces into vectors
+    // puts the 5 data pieces into vectors
     while(getline(myFileStream, line)) {
         if (line.length() < 58) { // skips first two lines
             stringstream ss(line);
@@ -60,54 +68,179 @@ int main(int argc, char * argv[]) {
             durations.push_back(duration);
         }
     }
-
-    cout << "sourcePIDs 1st: " << sourcePIDs.front()  << " last: " << sourcePIDs.back() << endl;
-
-    cout << "--------------------------------" << endl;
-
-    cout << "sourceActivities 1st: " << sourceActivities.front() << " last: " << sourceActivities.back() << endl;
-
-    cout << "--------------------------------" << endl;
-
-    cout << "targetPIDs 1st: " << targetPIDs.front() << " last: " << targetPIDs.back() << endl;
-
-    cout << "--------------------------------" << endl;
-
-    cout << "targetActivities 1st: " << targetActivities.front() << " last: " << targetActivities.back() << endl;
-
-    cout << "--------------------------------" << endl;
-
-    cout << "durations 1st: " << durations.front() << " last: " << durations.back() << endl;
-
-//        for(vector<int>::const_iterator iter=sourcePIDs.begin(); iter != sourcePIDs.end(); iter++){
-//            cout << *iter << endl;
-//        }
-//
-//        cout << "--------------------------------" << endl;
-//
-//        for(vector<int>::const_iterator iter=sourceActivities.begin(); iter != sourceActivities.end(); iter++){
-//            cout << *iter << endl;
-//        }
-//
-//        cout << "--------------------------------" << endl;
-//
-//        for(vector<int>::const_iterator iter=targetPIDs.begin(); iter != targetPIDs.end(); iter++){
-//            cout << *iter << endl;
-//        }
-//
-//        cout << "--------------------------------" << endl;
-//
-//        for(vector<int>::const_iterator iter=targetActivities.begin(); iter != targetActivities.end(); iter++){
-//            cout << *iter << endl;
-//        }
-//
-//        cout << "--------------------------------" << endl;
-//
-//        for(vector<int>::const_iterator iter=durations.begin(); iter != durations.end(); iter++){
-//            cout << *iter << endl;
-//        }
-
     myFileStream.close();
+
+    // make copy nodes for original nodes(IDs)
+    int beginRangeSourcePIDs = *min_element(sourcePIDs.begin(), sourcePIDs.end());
+    int endRangeSourcePIDs = *max_element(sourcePIDs.begin(), sourcePIDs.end());
+    int beginRangeTargetPIDs = *min_element(targetPIDs.begin(), targetPIDs.end());
+    int endRangeTargetPIDs = *max_element(targetPIDs.begin(), targetPIDs.end());
+    vector<int> totalCopies;
+    if(beginRangeSourcePIDs < beginRangeTargetPIDs && endRangeSourcePIDs < endRangeTargetPIDs){
+        totalCopies = newInterval(endRangeTargetPIDs, sourcePIDs.size() + targetPIDs.size());
+    }
+    else if(beginRangeSourcePIDs > beginRangeTargetPIDs && endRangeSourcePIDs < endRangeTargetPIDs){
+        totalCopies = newInterval(endRangeTargetPIDs, sourcePIDs.size() + targetPIDs.size());
+    }
+    else if(beginRangeSourcePIDs < beginRangeTargetPIDs && endRangeSourcePIDs > endRangeTargetPIDs){
+        totalCopies = newInterval(endRangeSourcePIDs, sourcePIDs.size() + targetPIDs.size());
+    }
+    else if(beginRangeSourcePIDs > beginRangeTargetPIDs && endRangeSourcePIDs > endRangeTargetPIDs){
+        totalCopies = newInterval(endRangeSourcePIDs, sourcePIDs.size() + targetPIDs.size());
+    }
+
+    // split copies into 2 vectors for "source copy" and "target copy" IDs
+    vector<int> sourceCopies, targetCopies;
+    vector<int>::iterator middleItr(totalCopies.begin() + totalCopies.size() / 2);
+    for(auto it = totalCopies.begin(); it != totalCopies.end(); it++){
+        if(distance(it, middleItr) > 0){
+            sourceCopies.push_back(*it);
+        }
+        else{
+            targetCopies.push_back(*it);
+        }
+    }
+
+    // rewiring edges -- generating random numbers
+    float amountOfRewiring1 = rewiringFactor * sourcePIDs.size(); // amount of edges to rewire
+    float amountOfRewiring2 = amountOfRewiring1;
+    srand(time(NULL)); // srand() seeds rand() automatically
+    // random original IDs
+    vector<int> originalRandomIDs;
+    for(int i = 0; i < amountOfRewiring1; i++){ // populate vector with random IDs to rewire
+        int random = (rand()%(endRangeSourcePIDs - beginRangeSourcePIDs)) + beginRangeSourcePIDs;
+        if(find(originalRandomIDs.begin(), originalRandomIDs.end(), random) != originalRandomIDs.end()){ // gets "rid", moves past duplicate random nums
+            amountOfRewiring1++;
+        }
+        else {
+            originalRandomIDs.push_back(random);
+        }
+    }
+    // random copy IDs
+    vector<int> copyRandomIDs;
+    for(int i = 0; i < amountOfRewiring2; i++){ // populate vector with random IDs to rewire
+        int random = (rand()%(sourceCopies.back() - sourceCopies.front())) + sourceCopies.front(); // maybe be: low+(rand()%(upper-lower+1))
+        if(find(copyRandomIDs.begin(), copyRandomIDs.end(), random) != copyRandomIDs.end()){ // gets "rid", moves past duplicate random nums
+            amountOfRewiring2++;
+        }
+        else {
+            copyRandomIDs.push_back(random);
+        }
+    }
+
+    cout << "-------------" << endl;
+    cout << "orginial PIDs before rewire" << endl;
+    cout << "source: ";
+    for(auto it = sourcePIDs.begin(); it != sourcePIDs.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << "target: ";
+    for(auto it2 = targetPIDs.begin(); it2 != targetPIDs.end(); it2++) {
+        cout  << *it2 << " ";
+    }
+    cout << endl;
+
+
+    cout << "-------------" << endl;
+    cout << "copy PIDs before rewire" << endl;
+    cout << "source: ";
+    for(auto it = sourceCopies.begin(); it != sourceCopies.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << "target: ";
+    for(auto it2 = targetCopies.begin(); it2 != targetCopies.end(); it2++) {
+        cout  << *it2 << " ";
+    }
+    cout << endl;
+
+    cout << "-------------" << endl;
+    cout << "random IDs generated" << endl;
+    cout << "original random IDs: ";
+    for(auto it = originalRandomIDs.begin(); it != originalRandomIDs.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << "copy random IDs: ";
+    for(auto it2 = copyRandomIDs.begin(); it2 != copyRandomIDs.end(); it2++) {
+        cout  << *it2 << " ";
+    }
+    cout << endl;
+
+    int indexOfOriginalSourcePID = 0;
+    int originalTargetPIDToSave = 0;
+    int indexOfCopySourcePID = 0;
+    int copyTargetPIDToSave = 0;
+    while(!originalRandomIDs.empty() && !copyRandomIDs.empty()){
+        // handles original IDs
+        vector<int>::iterator itOriginal = find(sourcePIDs.begin(), sourcePIDs.end(), originalRandomIDs.back()); // find ID in source ID vector
+        indexOfOriginalSourcePID = distance(sourcePIDs.begin(), itOriginal); // find index of source ID, now have the location of the edge -- randomly chosen
+        originalTargetPIDToSave = targetPIDs.at(indexOfOriginalSourcePID); // have source ID index, target at same index, get target number, store to replace
+
+        // handles copy IDs
+        vector<int>::iterator itCopy = find(sourceCopies.begin(), sourceCopies.end(), copyRandomIDs.back());
+        indexOfCopySourcePID = distance(sourceCopies.begin(), itCopy);
+        copyTargetPIDToSave = targetCopies.at(indexOfCopySourcePID); // store copy target ID, to replace
+
+        // replace -- make new edges
+        replace(targetPIDs.begin(), targetPIDs.end(), originalTargetPIDToSave, copyTargetPIDToSave); // replaces original target ID with copy target ID
+        replace(targetCopies.begin(), targetCopies.end(), copyTargetPIDToSave, originalTargetPIDToSave); // replaces copy target ID with original target ID
+
+        // condition for while loop
+        originalRandomIDs.pop_back();
+        copyRandomIDs.pop_back();
+    }
+
+    cout << "-------------" << endl;
+    cout << "orginial PIDs after rewire" << endl;
+    cout << "source: ";
+    for(auto it = sourcePIDs.begin(); it != sourcePIDs.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << "target: ";
+    for(auto it2 = targetPIDs.begin(); it2 != targetPIDs.end(); it2++) {
+        cout  << *it2 << " ";
+    }
+    cout << endl;
+
+    cout << "-------------" << endl;
+    cout << "copy PIDs after rewire" << endl;
+    cout << "source: ";
+    for(auto it = sourceCopies.begin(); it != sourceCopies.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << "target: ";
+    for(auto it2 = targetCopies.begin(); it2 != targetCopies.end(); it2++) {
+        cout  << *it2 << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
+/**
+ * Creates a new range for the copied nodes. Populates a vector with the new IDs.
+ * @param endRange
+ * @param vectorSize
+ * @return sourcePIDsCopy
+ */
+vector<int> newInterval(int endRange, int vectorSize){
+    int maxNewRangeSource = (endRange + 1) + (endRange - 1); // assumes continuous numbers for source/target IDs
+    vector<int> PIDsCopy;
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
+    while(count1 < maxNewRangeSource && count2 < numNodeCopies && count3 < vectorSize){
+        PIDsCopy.push_back(endRange + 1);
+        endRange++;
+        count1++;
+        count2++;
+        count3++;
+    }
+    return PIDsCopy;
+}
+
 
